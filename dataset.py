a485e2273b35e9e71ec40b8fba531e8ccf635782
ruxuanli <54103158+ruxuanli@users.noreply.github.com>
2019-08-15 10:47:07 +0800

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,43 @@
+from PIL import Image
+import torch
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+imsize = 512 if torch.cuda.is_available() else 128  # use small size if no gpu
+
+loader = transforms.Compose([
+    transforms.Resize((imsize,imsize)),  # scale imported image
+    transforms.ToTensor()])  # transform it into a torch tensor
+
+
+def image_loader(image_name):
+    image = Image.open(image_name)
+    # fake batch dimension required to fit network's input dimensions
+    image = loader(image).unsqueeze(0)
+    return image.to(device, torch.float)
+
+
+style_img = image_loader("spliting.jpg")
+content_img = image_loader("audrey3.jpg")
+print(style_img.shape)
+print(content_img.shape)
+assert style_img.size() == content_img.size(), \
+    "we need to import style and content images of the same size"
+
+unloader = transforms.ToPILImage()  # reconvert into PIL image
+
+plt.ion()
+
+
+def imshow(tensor, title=None):
+    image = tensor.cpu().clone()  # we clone the tensor to not do changes on it
+    image = image.squeeze(0)      # remove the fake batch dimension
+    image = unloader(image)
+    plt.imshow(image)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+
